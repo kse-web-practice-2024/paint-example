53fe0e49bc78f2f990c1f421c396d6cb7a09581d
denys-zavhorodnii <dzavhorodnii@kse.org.ua>
2024-11-07 00:19:40 +0200

    init

---
+++ main.js
@@ -0,0 +1,14 @@
+alert('Hello')
+
+/**
+ * ctx.lineCap = "round";
+ * ctx.lineWidth = number;
+ * ctx.strokeStyle = color;
+ *
+ * cts.beginPath()
+ * ctx.moveTo()
+ * ctx.lineTo()
+ * ctx.stroke()
+ *
+ * ctx.clearRect()
+ */
